e16981069f23c877013f9bf3a4bbe6bcbcb237d7
isabellechalhoub <isabelle.chalhoub@gmail.com>
2017-11-30 17:47:49 -0700

    intial logic for logging in a user is done. needs some cleaning up probably

--- giveitforward/src/main/java/giveitforward/models/Test.java
+++ giveitforward/src/main/java/giveitforward/models/Test.java
@@ -7,11 +7,10 @@ public class Test {
     public static void main(String[] args) {
         AnnotationConfiguration blah = new AnnotationConfiguration().configure();
         Session session = blah.buildSessionFactory().openSession();
-
         Transaction t = null;
         try {
             t = session.beginTransaction();
-            Tag t1 = new Tag("yeeeeesssss");
+            Tag t1 = new Tag("yay");
             session.save(t1);
             session.flush();
             t.commit();
@@ -20,7 +19,7 @@ public class Test {
             if (t != null) {
                 t.rollback();
             }
-            System.out.println("bad");
+            System.out.println("ROLLBACK");
         }
         finally {
             session.close();
---
+++ giveitforward/src/main/java/giveitforward/models/User.java
@@ -0,0 +1,87 @@
+package giveitforward.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "uid", unique = true, nullable = false)
+    private int uid;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "isadmin")
+    private boolean isadmin;
+
+    @Column(name = "oid")
+    private Integer oid;
+
+    @Column(name = "photo")
+    private String photo;
+
+    @Column(name = "bio")
+    private String bio;
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean getIsAdmin() {
+        return isadmin;
+    }
+
+    public void setIsadmin(boolean isadmin) {
+        this.isadmin = isadmin;
+    }
+
+    public Integer getOid() {
+        return oid;
+    }
+
+    public void setOid(Integer oid) {
+        this.oid = oid;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+}
